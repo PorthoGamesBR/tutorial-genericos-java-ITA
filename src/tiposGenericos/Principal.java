e1901ae852b93839962bfb06ebdd478ef7596f37
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-09 20:40:35 -0300

    Uso do metodo adicionaTodos na principal

--- src/tiposGenericos/Principal.java
+++ src/tiposGenericos/Principal.java
@@ -1,5 +1,8 @@
 package tiposGenericos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -9,8 +12,13 @@ public class Principal {
 		frutas.adiciona(new Fruta("Mamao"));
 		frutas.adiciona(new Fruta("Pera"));
 		frutas.adiciona(new Fruta("Jaca"));
-		
 		frutas.adiciona(new FrutaVermelha("Morango"));
+		
+		List<FrutaVermelha> vermelhas = new ArrayList<>(); 
+		vermelhas.add(new FrutaVermelha("Amora"));
+		vermelhas.add(new FrutaVermelha("Cereja"));
+		
+		frutas.adicionaTodos(vermelhas);
 		
 		while(frutas.temItems()) {
 			System.out.println(frutas.retira());
