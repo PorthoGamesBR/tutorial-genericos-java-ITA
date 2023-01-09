0b82699a1a8ffe2b7b87c434d6e21abaa77950f9
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-09 20:25:53 -0300

    Criada classe principal para testes

---
+++ src/tiposGenericos/Principal.java
@@ -0,0 +1,18 @@
+package tiposGenericos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Cesta<Fruta> frutas = new Cesta<>();
+		frutas.adiciona(new Fruta("Maca"));
+		frutas.adiciona(new Fruta("Banana"));
+		frutas.adiciona(new Fruta("Mamao"));
+		frutas.adiciona(new Fruta("Pera"));
+		frutas.adiciona(new Fruta("Jaca"));
+		
+		while(frutas.temItems()) {
+			System.out.println(frutas.retira());
+		}
+	}
+
+}
