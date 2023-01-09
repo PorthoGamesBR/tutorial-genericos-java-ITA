702b81003da4aa0c92a062f66b7edd931fa32491
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-09 20:34:49 -0300

    Criada classe FrutaVermelha

---
+++ src/tiposGenericos/FrutaVermelha.java
@@ -0,0 +1,16 @@
+package tiposGenericos;
+
+public class FrutaVermelha extends Fruta {
+	
+	public FrutaVermelha(String nome) {
+		super(nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Fruta Vermelha [nome:" + this.nome + "]";
+	}
+	
+	
+
+}
